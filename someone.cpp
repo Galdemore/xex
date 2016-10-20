bddc6d915ead8e64868cff93caec6154eec20d21
Galdemore <foretoldsoldier@gmail.com>
2016-10-19 23:24:13 -0400

    main
    this is the mainfile of the xex text game

---
+++ someone.cpp
@@ -0,0 +1,12 @@
+#include <cstdio>
+#include <iostream>
+using namespace std;
+bool quit = false;
+int main()
+{
+		while(!quit)
+		{
+			cout<<"In the year twenty one oh one, war was beginning, the trigger of this conflict was when cats set up them (captain an co) the bomb. This subsequently resulted in all of their base belonging to cats. However one Zig can move for great justice to turn the tide of the war. "<<endl;
+			cin.get();
+		}
+}
